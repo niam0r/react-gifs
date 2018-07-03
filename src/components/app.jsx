8c19669a297d5982be1988872894d90e5fa4128c
Romain niam0r <niamor@mm.st>
2018-07-03 19:59:09 +0100

    implemented Gif, displaying ok

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,12 +5,20 @@ import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedGifId: 'dbtDDSvWErdf2',
+      gifs: []
+    };
+  }
   render() {
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
-          <Gif />
+          <Gif id={this.state.selectedGifId}/>
         </div>
         <div className="right-scene">
           <GifList />
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 
 export default class Gif extends Component {
   render() {
+    let src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`
     return (
       <div>
-        Gif
+        <img src={src} alt="" className="selected-gif"/>
       </div>
     );
   }
