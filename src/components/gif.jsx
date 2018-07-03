a9948d216fddce24eecd8d6ce1bc10b4d6b00b68
Romain niam0r <niamor@mm.st>
2018-07-03 20:14:51 +0100

    added css classed and font awesome cdn

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -4,8 +4,8 @@ export default class Gif extends Component {
   render() {
     let src = `https://media.giphy.com/media/${this.props.id}/giphy.gif`
     return (
-      <div>
-        <img src={src} alt="" className="selected-gif"/>
+      <div className="selected-gif">
+        <img src={src} alt="" className="gif"/>
       </div>
     );
   }
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class GifList extends Component {
   render() {
     return (
-      <div>
+      <div className="gif-list">
         GifList
       </div>
     );
