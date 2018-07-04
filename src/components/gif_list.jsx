bda08b20a630141ad93c058a9534dfb0681d89c3
Romain niam0r <niamor@mm.st>
2018-07-04 11:45:31 +0100

    refactored with renderList" " "

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import Gif from './gif.jsx';
 
 export default class GifList extends Component {
-  render() {
+  renderList = () => {
     return this.props.gifs.map(gif => {
-      return <Gif id={gif.id} />
-    });
+      return <Gif id={gif.id} key={gif.id} />
+    })
+  }
+  render() {
+    return (
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
