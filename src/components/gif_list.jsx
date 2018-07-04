f187de0acc3f9b65e01b277309a7fa6dcefc9710
Romain niam0r <niamor@mm.st>
2018-07-04 11:47:21 +0100

    refactored 1 liner

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -3,9 +3,7 @@ import Gif from './gif.jsx';
 
 export default class GifList extends Component {
   renderList = () => {
-    return this.props.gifs.map(gif => {
-      return <Gif id={gif.id} key={gif.id} />
-    })
+    return this.props.gifs.map(gif => <Gif id={gif.id} key={gif.id} /> )
   }
   render() {
     return (
